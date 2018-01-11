66177ecda847eea9ce0822f8ef7d06e0f61405bc
Calliope <thecalliopecrashed@gmail.com>
2018-01-11 11:26:09 -0500

    [fish] Update `cmd` shortcut to save changes

--- config/fish/functions/cmd.fish
+++ config/fish/functions/cmd.fish
@@ -1,15 +1,20 @@
 function cmd
-  set file ~/.config/fish/functions/$argv[1].fish
+  dev
+    set file config/fish/functions/$argv[1].fish
 
-  if test -e $file
-    echo "$file exists. Opening for edits..."
-  else
-    echo "$file does not exist. Creating..."
-    alias $argv[1]='ls'
-    funcsave $argv[1]
-  end
+    if test -e $file
+      echo "$file exists. Opening for edits..."
+    else
+      echo "$file does not exist. Creating..."
+      alias $argv[1]='ls'
+      funcsave $argv[1]
+    end
 
-  reattach-to-user-namespace nvim $file
+    vi $file
 
-  source $file
+    test -e $file
+    and source $file
+    and g a $file
+    and g c $file
+  cd -
 end
