d3a2c2633d778eabf4e2223a64c8dcb661267f67
Calliope <thecalliopecrashed@gmail.com>
2018-01-11 12:04:21 -0500

    [fish] Add the time-tested battle-hardened `merge`
    See https://github.com/graysonwright/dotfiles/blob/master/bin/merge

---
+++ config/fish/functions/merge.fish
@@ -0,0 +1,16 @@
+function merge
+  set branch (git symbolic-ref HEAD --short)
+  echo "Merging branch `$branch`"
+
+  and verbose git fetch
+  and verbose git checkout $branch
+  and verbose git rebase -i origin/master
+  and verbose git push --force-with-lease
+  and verbose ./bin/docker-compose run web rspec
+  and verbose git checkout master
+  and verbose git pull
+  and verbose git merge --ff-only $branch
+  and verbose git push
+  and verbose git branch -d $branch
+  and verbose git push --force-with-lease origin :$branch
+end
